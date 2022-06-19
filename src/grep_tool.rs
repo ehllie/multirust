1a1460b9d093996ce52c9627fd6b7ae5409905d1
Elizabeth Paź <ellieeepaz@gmail.com>
2022-06-19 13:34:25 +0200

    Chapter 12 done

---
+++ src/grep_tool.rs
@@ -0,0 +1,65 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+
+pub struct Config<'a> {
+    pub query: &'a String,
+    pub filename: &'a String,
+    pub case_sensitive: bool,
+}
+
+impl<'a> Config<'a> {
+    pub fn new(args: &'a [String]) -> Result<Config<'a>, &'static str> {
+        if args.len() < 4 {
+            return Err("Grep tool requires 2 arguements, query and filename!");
+        }
+
+        let query = &args[2];
+        let filename = &args[3];
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // --snip--
+    let contents = fs::read_to_string(config.filename)?;
+
+    if config.case_sensitive {
+        for line in search(config.query, &contents) {
+            println!("{}", line)
+        }
+    } else {
+        for line in search_case_insensitive(config.query, &contents) {
+            println!("{}", line)
+        }
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        };
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        };
+    }
+    results
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
+use multirust::grep_tool;
 use std::env;
-
-fn grep(query: &String, filename: &String) {
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-}
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let tool = &args[1];
-    match tool.as_str() {
-        "grep" => grep(&args[2], &args[3]),
-        _ => println!("Unknown tool: {}", tool),
+    let tool = parse_tool(&args).unwrap_or_else(|err| {
+        eprintln!("Error while selecting tool: {}", err);
+        process::exit(1)
+    });
+
+    match tool {
+        Tool::Grep => grep(&args),
+    }
+}
+
+enum Tool {
+    Grep,
+}
+
+fn parse_tool(args: &[String]) -> Result<Tool, &'static str> {
+    if args.len() < 2 {
+        return Err("A tool name needs to be provided!");
+    }
+    match args[1].as_str() {
+        "grep" => Ok(Tool::Grep),
+        _ => Err("Unknown tool"),
+    }
+}
+
+fn grep(args: &[String]) {
+    let config = grep_tool::Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Error while parsing config: {}", err);
+        process::exit(1)
+    });
+
+    if let Err(e) = grep_tool::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1)
     }
 }
